2598d896ce0dd1f9507652d285ea472e6cea905c
kwangho.woo <wookh1@gmail.com>
2017-10-19 22:21:31 +0900

    class4

--- class4_script.R
+++ class4_script.R
@@ -33,3 +33,50 @@ airports %>%
 airports %>%
   filter(grepl("[[:digit:]]$", name))
 
+
+# stringr 
+if (!require(tidyverse)) install.packages("tidyverse")
+library(tidyverse)
+library(stringr)
+
+str_length(c("abc","한글","ㅎㄱㅏ"))
+
+names(iris)[5] <- "Category"
+names(iris)
+
+strings <- c(
+  "apple",
+  "219 733 8965",
+  "329-293-8753",
+  "Work: 579-499-7527; Home: 543.355.3679"
+)
+
+phone <- "([2-9][0-9]{2})[- .]([0-9]{3})[- .]([0-9]{4})"
+str_subset(strings, phone)
+
+strings[grep(phone, strings)]
+str_detect(strings, phone)
+
+# 패턴이 나온 횟수
+str_count(strings, phone)
+
+str_locate(strings, phone)
+
+str_locate_all(strings, phone)
+str_extract_all(strings, phone, simplify = T)
+
+#데이터 프레임으로 변환 
+my_data <- do.call(rbind.data.frame, str_match_all(strings, phone))
+
+strings <- c(
+  "543-355-3679",
+  "219-733-8965",
+  "329-293-8753",
+  "579-499-7527"
+  )
+
+
+lapply(tem, function(x) x[1])
+
+getFirst <- function(x) {x[1]}
+lapply(tem, getFirst)
